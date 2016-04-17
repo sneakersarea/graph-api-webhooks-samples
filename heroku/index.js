58cb7cb463e844eeed3edbfb68591a40aa7aba80
sneakersarea <sneakersareaid@gmail.com>
2016-04-17 09:33:58 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -45,4 +45,17 @@ app.post('/instagram', function(req, res) {
   res.sendStatus(200);
 });
 
+app.post('/facebook', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.listen();
