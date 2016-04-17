6f74386645bd988872e957aaaf50fa95d1663239
sneakersarea <sneakersareaid@gmail.com>
2016-04-17 10:01:33 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -52,7 +52,13 @@ app.post('/facebook', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-       sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      if (text === 'Generic') {
+        sendGenericMessage(sender);
+        continue;
+      } else {
+         sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      }
+    }
     }
   }
   res.sendStatus(200);
@@ -64,6 +70,55 @@ function sendTextMessage(sender, text) {
   messageData = {
     text:text
   }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token:token},
